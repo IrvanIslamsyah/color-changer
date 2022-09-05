35e09d9fc088bcac2d12b197c58807671ffbbefb
IrvanIslamsyah <m.irfanislmsyh@gmail.com>
2022-09-05 07:33:23 +0700

    upload pertama

---
+++ belajar_function.js
@@ -0,0 +1,20 @@
+// // Global Variabel
+// let i = 10
+
+// // Fucntion
+// function panggilAyang(){
+//     // Local Variabel
+//     let x = 20
+//     console.log(i)
+// }
+// // Memanggil Function
+// console.log(i)
+// console.log(x)
+// panggilAyang()
+
+// Parameter
+function tambah(a, b){
+    console.log(a+b)
+}
+
+tambah(2,2)
